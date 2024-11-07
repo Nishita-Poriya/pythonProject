cdee1a14218feff3307fc8b65f05355bd65dfa3e
Kaushal <kaushal07@Kaushals-MacBook-Air.local>
2024-11-07 08:01:50 +0530

    Initial commit

---
+++ Keywords&Variables/Lab006_Variables_Types.py
@@ -0,0 +1,7 @@
+age = 19
+name ="Nishita"
+x="True"
+
+print("age=",age)
+print("name",name)
+print("x=",x)
---
+++ PyATBLearning/Nov/ex_05112024_Python_Basics/Lab003_Python_Basics.py
@@ -0,0 +1,16 @@
+print("hello world")
+#
+print(2 + 2)
+
+
+```
+# print(, *args, sep=' ', end='\n', file=None):
+# *args - unlimited number of comma seperated arguments
+#     print("Pramod Dutta",123,"Amit","John") # IndentationError: unexpected indent
+```
+
+print("Nishita",123,"Poriya","star")
+print("Nishita",123,"Poriya","star",sep='*')
+print("Nishita",123,"Poriya","star",sep='*',end='  __ ')
+
+
