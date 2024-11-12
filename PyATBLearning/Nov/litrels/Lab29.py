d1c92ecedd04a49a11129df559939fd086c8c1ba
Kaushal <kaushal07@Kaushals-MacBook-Air.local>
2024-11-12 13:19:54 +0530

    DIY FILES ADDED

---
+++ PyATBLearning/Nov/litrels/Lab29.py
@@ -0,0 +1,25 @@
+# Task for the Today
+# Take a 3 input from the user
+# perform the sub, sub, mul and div
+
+# Logic Building Formula
+
+
+num1 = float(input("Enter value1"))
+num2 = float(input("Enter value2"))
+num3 = float(input("Enter value3"))
+
+#perfoming operations/logic
+sum =num1+num2+num3
+sub =num1-num2-num3
+mul=num1*num2*num3
+div=(num1+num2)/num3
+
+
+#print users o/p
+print("sum",sum)
+print("sub",sub)
+print("mul",mul)
+print("Div",div)
+
+
---
+++ PyATBLearning/Nov/litrels/Lab30.py
@@ -0,0 +1,17 @@
+# Task for the Today
+# Take a 2 input from the user
+# Print the Quotient and Remainder
+# 15 ->  num1
+# 2 -> num2
+# Q -> 7
+# R -> 1
+
+
+num1 = int(input("Enter the number1"))
+num2 = int(input("Enter the number2"))
+
+quotient = num1 // num2
+Reminder = num1 % num2
+
+print("quotient =",quotient)
+print("Reminder=",Reminder)
