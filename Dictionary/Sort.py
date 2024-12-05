4ffffe1c2047874fd5d189c9b03814b5beb79c45
Nishita Poriya <nishita.poria@gmail.com>
2024-12-05 20:26:20 +0530

    sorted dict values

---
+++ Dictionary/Sort.py
@@ -0,0 +1,11 @@
+
+
+# Sorting by values in descending order
+
+
+
+my_dict={"a":3,"b":2,"c":1}
+
+sorted_dict= dict(sorted(my_dict.items(),key = lambda item: item[1],reverse=True))
+
+print(sorted_dict)
