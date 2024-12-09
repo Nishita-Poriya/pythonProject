683b0fba5acd0ea3bcdf872fd00f77025d5422ef
Nishita Poriya <nishita.poria@gmail.com>
2024-12-09 13:20:18 +0530

    structured

--- Lamda_List_Tuples/Lab092_Lamda.py
+++ Lamda_List_Tuples/Lab092_Lamda.py
@@ -1,8 +1,3 @@
-from Lamda_List_Tuples.Lab090_Lamda import result_l
-from Loops.Lab060_Match_stmt import browser
-from PyATBLearning.Nov.litrels.Lab019 import result
-
-
 def mul(a, b):
     return a * b
 
---
+++ Nov_Basic_Python/PyATBLearning/Nov/litrels/Lab25.py
@@ -0,0 +1,10 @@
+name ="This is a big line"
+print(type(name))
+name=name + str(1). # can only concatenate str (not "int") to str
+
+print(name)
+
+firstname ="Nishita"
+Lastname="Poriya"
+full_name =firstname + "  " +Lastname
+print("full_name",full_name)
